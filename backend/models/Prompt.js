7d18fc3ce35aa75b19c013cd3c67a0a045f27082
Abbas <abbasvajwana1gmai.com>
2026-01-06 00:30:01 +0530

    new folder

--- backend/models/Prompt.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const PromptSchema = new mongoose.Schema({
-    prompt: String,
-    response: String,
-    createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Prompt", PromptSchema);
